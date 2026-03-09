a97f33ec6be66b800915a1002ced47701e3eb0e0
Joao Vitor Caetano <joaovitorcaetano2808@gmail.com>
2026-03-09 18:32:13 -0300

    Add files via upload

---
+++ exceptions2-java/src/entities/Account.java
@@ -0,0 +1,69 @@
+package entities;
+
+import exceptions.BusinessException;
+
+public class Account {
+
+    private Integer number;
+    private String holder;
+    private Double balance;
+    private Double withdrawLimit;
+
+    public Account(){
+    }
+
+    public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+        if (balance < withdrawLimit) {
+            throw new BusinessException("The withdraw Limit can't be less than " + withdrawLimit);
+        }
+
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+        this.withdrawLimit = withdrawLimit;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public Double getWithdrawLimit() {
+        return withdrawLimit;
+    }
+
+    public void setWithdrawLimit(Double withdrawLimit) {
+        this.withdrawLimit = withdrawLimit;
+    }
+
+    public void deposit(Double amount){
+        this.balance += amount;
+    }
+
+    public void withdraw(Double amount){
+        if (amount > balance) {
+            throw new BusinessException("Not enough balance");
+        }
+        if (amount > withdrawLimit) {
+            throw new BusinessException("The amount exceeds withdraw limit");
+        }
+
+        this.balance -= amount;
+    }
+
+}
